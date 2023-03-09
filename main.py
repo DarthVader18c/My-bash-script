0f6ce2ef35474149c635ceef3a86782dc1e48ba9
DarthVader18c <103878452+DarthVader18c@users.noreply.github.com>
2023-03-09 20:36:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
-
-sides = sorted(sides, reverse=True)
-
-smax = 0
-for i in range(len(sides)):
-    for j in range(i + 1, len(sides)):
-        for k in range(j + 1, len(sides)):
-            a = sides[i]
-            b = sides[j]
-            c = sides[k]
-            if a + b > c and a + c > b and b + c > a:
-                p = (a + b + c) / 2
-                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-                if s > smax:
-                    smax = s
-
-print("Максимальная площадь треугольника", smax)
+import requests
+city = "Moscow,RU"
+appid = "1df7ead22466bee19cfb42e4122c41e9"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+             params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Город:", city)
+print("Скорость ветра:", data['wind']['speed'])
+print("Дальность", data['visibility'])
